f7e7ee169fa252572f9d0a76a81971177a1ecd6f
Kevin Reid <kevinnivek@me.com>
2022-08-21 12:51:27 -0400

    added individual project views

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -26,4 +26,22 @@ router.get("/employee_tracker", async (req, res) => {
     });
 });
 
+
+router.get("/tech_blog", async (req, res) => {
+
+    res.render("tech_blog", {
+    });
+});
+
+router.get("/iBetter", async (req, res) => {
+
+    res.render("iBetter", {
+    });
+});
+
+router.get("/weather_dashboard", async (req, res) => {
+
+    res.render("weather_dashboard", {
+    });
+});
   module.exports = router;
